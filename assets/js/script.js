4d3a3f8472b31159b5f2ae87d9a4149b2aca725b
diegorodd <dieegoa@icloud.com>
2022-06-04 18:08:44 -0400

    added on js every hour with its storage/value

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,14 @@ $(document).ready(function () {
         localStorage.setItem(time,text);
    })
 })
+
+$("#hour7 .description").val(localStorage.getItem("hour 7"));
+$("#hour8 .description").val(localStorage.getItem("hour 8"));
+$("#hour9 .description").val(localStorage.getItem("hour 9"));
+$("#hour10 .description").val(localStorage.getItem("hour 10"));
+$("#hour11 .description").val(localStorage.getItem("hour 11"));
+$("#hour12 .description").val(localStorage.getItem("hour 12"));
+$("#hour13 .description").val(localStorage.getItem("hour 13"));
+$("#hour14 .description").val(localStorage.getItem("hour 14"));
+$("#hour15 .description").val(localStorage.getItem("hour 15"));
+$("#hour16 .description").val(localStorage.getItem("hour 16"));
