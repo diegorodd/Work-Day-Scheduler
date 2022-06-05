89badbb1015ec9b7b580c28bc3a33206c02ff477
diegorodd <dieegoa@icloud.com>
2022-06-04 21:37:17 -0400

    added the timetracker function to run

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,16 +25,16 @@ $(document).ready(function () {
    })
 })
 // make sure to do military time for normal time
-$("#hour7 .description").val(localStorage.getItem("hour 7"));
-$("#hour8 .description").val(localStorage.getItem("hour 8"));
-$("#hour9 .description").val(localStorage.getItem("hour 9"));
-$("#hour10 .description").val(localStorage.getItem("hour 10"));
-$("#hour11 .description").val(localStorage.getItem("hour 11"));
-$("#hour12 .description").val(localStorage.getItem("hour 12"));
-$("#hour13 .description").val(localStorage.getItem("hour 13"));
-$("#hour14 .description").val(localStorage.getItem("hour 14"));
-$("#hour15 .description").val(localStorage.getItem("hour 15"));
-$("#hour16 .description").val(localStorage.getItem("hour 16"));
+$("#hour7 .description").val(localStorage.getItem("hour7"));
+$("#hour8 .description").val(localStorage.getItem("hour8"));
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
 // would make a loop
 function timeTracker() {
      var timeNow = moment().hour();
@@ -58,5 +58,4 @@ function timeTracker() {
             $(this).assClass("future");
          }
      })
-     timeTracker();
-}
+}    timeTracker();
